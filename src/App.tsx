420ec844b346f07454a00b4db9a70cbd07f6e6b9
Murhussain <husseinmurashi@yahoo.com>
2024-05-02 23:49:21 +0200

    Initial Project setup and tailwindCSS configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import HomePage from "./pages/home.tsx";
-function App() {
+import AppRoutes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+export default function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p className="text-2xl text-amber-700">Mur Paragraph test tailwindCSS</p>
-          <p>Home content</p>
-            <HomePage />
-      </div>
-    </>
+      <BrowserRouter>
+          <div className="h-screen">
+              <AppRoutes />
+          </div>
+      </BrowserRouter>
   )
 }
-
-export default App
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,10 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home of mur hussein</h1>
-    </div>
+      <>
+          <div className="h-full bg-amber-100 flex items-center justify-center">
+              <h1 className="text-red-400">Home of mur hussein</h1>
+
+          </div>
+      </>
   );
 }
