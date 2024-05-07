42c836c59cf9aff80e67b487b317534d8cf7925b
Murhussain <husseinmurashi@yahoo.com>
2024-05-07 21:41:00 +0200

    Done with understanding of routes definitions

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-red-400">About</h1>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,11 @@ import HomePage from "../pages/home.tsx";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
   );
 }
 
